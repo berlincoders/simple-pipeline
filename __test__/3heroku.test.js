c56dc67b76da432a35189bc07c8d9dcd438796f7
Romeo <ralonsoad@gmail.com>
2024-03-08 09:26:34 +0100

    Adding test

--- __test__/3heroku.test.js
+++ __test__/3heroku.test.js
@@ -1,20 +1,18 @@
 const request = require('supertest');
-require('dotenv').config();  
-
 
 describe("Production Application Test", () => {
   it("should check if the application is running on production", async () => {
-    // Replace 'your-heroku-app-url' with the actual URL of your Heroku app
-    const herokuAppUrl = process.env.HEROKU_APP_URL;
+    // Retrieve test URL from environment variable
+    const testUrl = process.env.HEROKU_APP_URL;
 
-    // Sending a GET request to a specific endpoint of your application
-    const res = await request(herokuAppUrl)
-      .get('/api/health') // Replace '/api/health' with the actual health check endpoint of your application
+    // Sending a GET request to the test URL
+    const res = await request(testUrl)
+      .get('/')
       .set('Accept', 'application/json');
 
-
-    // Expecting the status code of the response to be 200
-    expect(res.statusCode).toEqual(200);
+    // Expecting the status code of the response to indicate success (2xx range)
+    expect(res.statusCode).toBeGreaterThanOrEqual(200);
+    expect(res.statusCode).toBeLessThan(300); // Ensure status code is in the 2xx range
 
     // You can add more assertions here to further verify if the application is running correctly
   });
